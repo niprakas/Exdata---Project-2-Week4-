d89ebd25be767f3a7a049f7277c64a0acbcb594b
niprakas <nikhilprakash.nik@gmail.com>
2018-04-12 16:00:51 +0530

    Add files via upload

--- plot6.R
+++ plot6.R
@@ -31,7 +31,7 @@ plot6<- function(){
 	## ggplot
 	  png("plot6.png", width=840, height=480)
 	  g <- ggplot(totalEmissions, aes(factor(year), Emissions))
-	  g + facet_grid(. ~ fips)
+	  g <- g + facet_grid(. ~ fips)
 	  g <- g + geom_bar(stat="identity")  + xlab("year") + ylab(expression('Total PM'[2.5]*" Emissions")) +
   	  ggtitle('Total Emissions in Baltimore City, MD (fips = "24510") vs Los Angeles, CA (fips = "06037")  1999-2008')
 	  print(g)
